d4678fcea151965f13763217d36829b440bd5f82
Michelle Plarina <micher@rescuityonline.com>
2013-10-01 16:07:27 -0600

    Files

---
+++ Assignment1/Assignment1/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment1.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Models.LoginModel Model)
+        {
+            if (Model.Email == "whatever" && Model.Password == "whatever")
+            {
+                System.Web.Security.FormsAuthentication.RedirectFromLoginPage(Model.Email, true);
+                
+            }
+            return null;
+        }
+        public ActionResult Logout()
+        {
+            System.Web.Security.FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
