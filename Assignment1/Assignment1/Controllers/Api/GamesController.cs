74b90a6c65001d97ba9fa5b86738af1d2fa931db
Michelle Plarina <micher@rescuityonline.com>
2013-10-08 16:00:53 -0600

    Added edit button and update button to games page

---
+++ Assignment1/Assignment1/Controllers/Api/GamesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment1.Controllers.Api
+{
+    [RoutePrefix("api")]
+    public class  GamesController : ApiController 
+    {
+        [HttpGet("Games")]
+        public List<Models.DTO.Game> GetGames()
+        {
+            MichelleEntities context = new MichelleEntities();
+
+            return (from g in context.Games 
+                         select new Models.DTO.Game { 
+                             id = g.ID,
+                             name = g.Name
+                         }).ToList();
+            
+        }
+        [HttpPost("games")]
+        public Models.DTO.Game AddGame(Models.DTO.Game game)
+        {
+            MichelleEntities context = new MichelleEntities();
+
+            Game newGame = new Game
+            {
+                Name = game.name
+            };
+
+            context.Games.Add(newGame);
+            context.SaveChanges();
+
+            return new Models.DTO.Game
+            {
+                id = newGame.ID,
+                name = newGame.Name
+            };
+        }
+        [HttpPut("games/{id}")]
+        public void UpdateGame(int id, Models.DTO.Game gameInfo)
+        {
+            MichelleEntities context = new MichelleEntities();
+            Game game = (from g in context.Games
+                         where g.ID == id
+                         select g).FirstOrDefault();
+            if (game != null)
+            {
+                game.Name = gameInfo.name;
+
+                context.SaveChanges();
+
+            }
+
+        }
+    }
+
+}
---
+++ Assignment1/Assignment1/Models/DTO/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment1.Models.DTO
+{
+    public class Game
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public string editButton { get; set; }
+    }
+}
